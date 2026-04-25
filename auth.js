96f083aa253ba8f0631fb32fe72471ba52267138
baynna <hayderkareem654@gmail.com>
2026-04-25 19:29:29 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,20 @@
-console.log("auth working");
+function login() {
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value.trim();
+
+  if (!email || !password) {
+    alert("اكتب البريد وكلمة المرور");
+    return;
+  }
+
+  auth.signInWithEmailAndPassword(email, password)
+    .catch(function(error) {
+      alert(error.message);
+    });
+}
+
+function logout() {
+  auth.signOut().then(function () {
+    location.reload();
+  });
+}
