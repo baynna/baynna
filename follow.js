6d21121a163fed625d41584b276618f8f4686bc8
baynna <hayderkareem654@gmail.com>
2026-05-08 19:54:58 +0300

    Update follow.js

--- follow.js
+++ follow.js
@@ -3,64 +3,69 @@ const db = firebase.firestore();
 // متابعة
 function followUser(targetUserId) {
 
-  const user = firebase.auth().currentUser;
+const user = firebase.auth().currentUser;
 
-  if (!user) {
-    alert("سجل دخول أولاً");
-    return;
-  }
+if (!user) {
+alert("سجل دخول أولاً");
+return;
+}
 
-  db.collection("follows")
-    .doc(user.uid + "_" + targetUserId)
-    .set({
-      follower: user.uid,
-      following: targetUserId
-    })
-    .then(() => {
-      loadFollowButton(targetUserId);
-    });
+db.collection("follows")
+.doc(user.uid + "_" + targetUserId)
+.set({
+follower: user.uid,
+following: targetUserId
+})
+.then(function() {
+loadFollowButton(targetUserId);
+});
 
 }
 
 // إلغاء متابعة
 function unfollowUser(targetUserId) {
 
-  const user = firebase.auth().currentUser;
+const user = firebase.auth().currentUser;
 
-  db.collection("follows")
-    .doc(user.uid + "_" + targetUserId)
-    .delete()
-    .then(() => {
-      loadFollowButton(targetUserId);
-    });
+if (!user) return;
+
+db.collection("follows")
+.doc(user.uid + "_" + targetUserId)
+.delete()
+.then(function() {
+loadFollowButton(targetUserId);
+});
 
 }
 
 // حالة الزر
 function loadFollowButton(targetUserId) {
 
-  const user = firebase.auth().currentUser;
-  if (!user) return;
+const user = firebase.auth().currentUser;
+if (!user) return;
 
-  const btn = document.getElementById("followBtn");
+const btn = document.getElementById("followBtn");
+if (!btn) return;
 
-  db.collection("follows")
-    .doc(user.uid + "_" + targetUserId)
-    .get()
-    .then(doc => {
+db.collection("follows")
+.doc(user.uid + "_" + targetUserId)
+.get()
+.then(function(doc) {
 
-      if (doc.exists) {
-        btn.innerText = "إلغاء المتابعة";
-        btn.onclick = function() {
-          unfollowUser(targetUserId);
-        };
-      } else {
-        btn.innerText = "متابعة";
-        btn.onclick = function() {
-          followUser(targetUserId);
-        };
-      }
+```
+  if (doc.exists) {
+    btn.innerText = "إلغاء المتابعة";
+    btn.onclick = function() {
+      unfollowUser(targetUserId);
+    };
+  } else {
+    btn.innerText = "متابعة";
+    btn.onclick = function() {
+      followUser(targetUserId);
+    };
+  }
 
-    });
+});
+```
 
 }
