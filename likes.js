a507b3697a2f54802d0ed2e11ec21577d7286982
baynna <hayderkareem654@gmail.com>
2026-06-06 00:58:58 +0300

    Update likes.js

--- likes.js
+++ likes.js
@@ -1,7 +1,30 @@
+// =======================
+// لايك المنشور (كما هو)
+// =======================
 function likePost(postId) {
   db.collection("posts").doc(postId).update({
     likes: firebase.firestore.FieldValue.increment(1)
   }).then(() => {
     renderPosts();
+  }).catch((e) => {
+    console.log(e);
   });
+}
+
+
+// =======================
+// لايك التعليق (إضافة جديدة)
+// =======================
+function likeComment(commentId) {
+
+  db.collection("comments").doc(commentId).update({
+    likes: firebase.firestore.FieldValue.increment(1)
+  })
+  .then(() => {
+    console.log("comment liked");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
 }
